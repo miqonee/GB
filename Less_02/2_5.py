444a915bef525b30c7ba7d7744b258359d088ce1
miqonee <108046712+miqonee@users.noreply.github.com>
2022-07-22 21:47:54 +0700

    Update 2_5.py
    Исправил ввод

--- Less_02/2_5.py
+++ Less_02/2_5.py
@@ -4,10 +4,11 @@ my_list = [7, 8, 4, 6, 1]
 print(f"Изначальный списко выглядит так {my_list}")
 while q !='q':
     q = input('Введите q для выхода из программы, или любой другой символ для продолжения')
-    if q !='q':
+    for el in range(len(my_list)):
+     if q !='q':
         i = int(input("Введите число"))
-        my_list.append(i)
+        my_list.insert(el + 1, i)
         my_list.sort()
         print(my_list[::-1])
-    elif q == "q":
-        print("Пока пока")
+     elif q == "q":
+         print("Пока пока")
